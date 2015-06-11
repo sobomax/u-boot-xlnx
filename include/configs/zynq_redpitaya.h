e0b4d6e2b2a6aaacb26dbdf5a0988971fcf6f5b0
Maksym Sobolyev <sobomax@sippysoft.com>
2015-06-10 21:31:09 -0700

    Add RedPitaya support. Patch by  Jeremy Herbert, http://bit.ly/1BZPQ3e

---
+++ include/configs/zynq_redpitaya.h
@@ -0,0 +1,29 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ *
+ * Configuration for Zynq Evaluation and Development Board - ZedBoard
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_ZED_H
+#define __CONFIG_ZYNQ_ZED_H
+
+#define CONFIG_SYS_SDRAM_SIZE		(512 * 1024 * 1024)
+#define CONFIG_SYS_FREQ_HZ		666666687
+#define CONFIG_ZYNQ_PS_CLK_FREQ		33333333UL
+
+#define CONFIG_ZYNQ_SERIAL_UART0
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ZYNQ_SDHCI0
+
+#define CONFIG_ZYNQ_BOOT_FREEBSD
+
+
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_ZED_H */
